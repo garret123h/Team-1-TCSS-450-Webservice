3d7c3f1b907b92c072e665788c8ee338dba73f4b
garret123h <gtullio12@protonmail.com>
2021-05-03 13:52:18 -1000

    Format code.

--- test/registrationTest.js
+++ test/registrationTest.js
@@ -7,48 +7,48 @@ chai.use(require('chai-http'));
 const app = require('../index.js'); // Our app
 
 var registrationBody = {
-    "first":"test",
-    "last":"test",
-    "email":"test@fake.email.com",
-    "password":"test"
+    "first": "test",
+    "last": "test",
+    "email": "test@fake.email.com",
+    "password": "test"
 };
 
 var signinBody = {
-    "username":"test",
-    "password":"password"
+    "username": "test",
+    "password": "password"
 }
 
 describe('Home page endpoint', function () {
-    it('should load the home page', function() {
+    it('should load the home page', function () {
         return chai.request(app)
-        .get('/')
-        .then(function(res) {
-            expect(res).to.have.status(200);
-        })
+            .get('/')
+            .then(function (res) {
+                expect(res).to.have.status(200);
+            })
     })
 })
 
 describe('Registration endpoint', function () {
     // POST register fake account.
-    it('should try and register user', function() {
+    it('should try and register user', function () {
         return chai.request(app)
-        .post('/auth')
-        .send(registrationBody)
-        .then(function(res) {
-            expect(res).to.have.status(200)
-          });
+            .post('/auth')
+            .send(registrationBody)
+            .then(function (res) {
+                expect(res).to.have.status(200)
+            });
     });
 }
 )
 
-describe('Email verification endpoint', function() {
+describe('Email verification endpoint', function () {
     // GET request for email verification
-    it('should retrieve email verification code', function(){
+    it('should retrieve email verification code', function () {
         return chai.request(app)
-        .get('/get-verification', function(err, response, body) {
-            response.statusCode.should.equal(200)
-            body.should.include(process.env.EMAIL_VERIFICATION)
-            done()
-        })
+            .get('/get-verification', function (err, response, body) {
+                response.statusCode.should.equal(200)
+                body.should.include(process.env.EMAIL_VERIFICATION)
+                done()
+            })
     })
 })
