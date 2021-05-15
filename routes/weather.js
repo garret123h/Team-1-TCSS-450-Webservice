13d1bf6349059be05c3bb0b34159a531f837d089
garret123h <gtullio12@protonmail.com>
2021-05-14 16:41:57 -1000

    Implement forecast endpoint.
    This change addresses the need by first building the endpoint to hit,
    then using the 'request' module to retrieve the forecast data. The data
    that is sent back is a list of forecasts. The list has a forecast for
    every 3 hours. Since we are returning 5 forecasts total then the program
    will iterate every 8 forecasts(every 24 hours) and extract the min
    temperature, max temperature, and a weather description for the day.
    There is also a partition function used for partitioning the array into
    lists of size 8.

--- routes/weather.js
+++ routes/weather.js
@@ -67,4 +67,84 @@ router.get('/current-weather/:zipcode', (request, res) => {
     }
 })
 
+/**
+ *  @api {get} /current-weather/forecast/:zipcode Get 5 day weather forecast
+ *  @apiName FiveDayForecast
+ *  @apiGroup Weather
+ * 
+ *  @apiParam {String} zipcode location of weather to be retrieved
+ *  @apiParamExample {String} https://group1-tcss450-project.herokuapp.com/weather/forecast/98030
+ * 
+ *  @apiSuccess {Success 200} {
+ *      City name,
+ *      list : [{
+ *          Weather Description, 
+ *          Temperature, 
+ *          Min Temperature, 
+ *          Max Temperature,
+ *          Date
+ *      }, ...
+ *      ]
+ *  }
+ *  @apiError {400: Missing Parameters} {String} message "Missing required information"
+ *  @apiError {400: Invalid zipcode} {String} message "Invalid zipcode"
+ *  @apiError {400: other Error} {String} message "other error, see detail"
+ *  @apiError (400: Other Error) {String} detail Information about th error
+ */
+ router.get('/forecast/:zipcode', (request, res) => {
+    // First validate the zipcode first.
+    if (isValidZipcode(request.params.zipcode)) {
+        let endpoint = 'https://' + weatherApiEndpointForecast + 'zip=' + request.params.zipcode +
+            ',us&appid=' + weatherAPIKey + '&units=imperial'
+
+        // Send API request to weather API
+        weatherRequest(endpoint, function (err, response, body) {
+            if (err) {
+                console.log(err)
+            } else {
+                // Extract the results
+                let json = JSON.parse(body)
+
+                // Since the API returns 3 hours interval forecasts. We want to iterate through 
+                // the list by day. So group each forecast into a group of 8. Since 3*8=24
+                let paritionForecastsByDay = partition(json.list, 8)
+
+                let mapResults = paritionForecastsByDay.map(forecast => {
+                    // From each list. Extract the weather description, Min and Max temperature
+                    var minTempDay = 1000, maxTempDay = 0
+
+                    forecast.forEach(interval => { // 3 hour interval forecast
+                        if (interval.main.temp_min < minTempDay) 
+                            minTempDay = interval.main.temp_min
+                        if (interval.main.temp_max > maxTempDay)
+                            maxTempDay = interval.main.temp_max
+                    })
+                    let weatherDescription = forecast[5].weather[0].description // Get the weather description for 12:00pm-3:00pm interval
+
+                    return {
+                        'Weather Description': weatherDescription,
+                        'Min Temperature': minTempDay,
+                        'Max Temperature': maxTempDay
+                    }
+                })
+
+                res.send({
+                    '5 day forecasts': mapResults,
+                    'City': json.city.name
+                })
+            }
+        })
+
+    } else {
+        response.status(400).send({
+            message: "Invalid zipcode"
+        })
+    }
+
+})
+
+function partition(array, n) {
+    return array.length ? [array.splice(0, n)].concat(partition(array, n)) : [];
+}
+
 module.exports = router;
