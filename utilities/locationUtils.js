0a54772373edbb74b2e89716402f91dc00d5ca85
garret123h <gtullio12@protonmail.com>
2021-05-17 13:16:25 -1000

    Implement utilities class for location
    This change addresses the need by implementing a zipcode to (latitude,
    longitude) as well as a couple of functions to convert the
    timezone/timezone offset to the proper local time.

---
+++ utilities/locationUtils.js
@@ -0,0 +1,46 @@
+// Used for local time conversion
+const moment = require('moment-timezone');
+
+const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('utilities/lookup_LatLong.txt')
+});
+
+// Create Map object for quick lookup
+const map = new Map()
+
+rl.on('line', (line) => {
+    let zipFromFile = line.split(',');
+    map[zipFromFile[0]] = [zipFromFile[1], zipFromFile[2]]
+});
+
+/**
+ * Converts a zipcode into the longitude and latitude
+ * @param {String} zipcode The zipcode to convert to longitude and latitude
+ */
+let getLongLatFromZipcode = (zipcode) => {
+    return map[zipcode]
+}
+
+/** 
+ * function to calculate local time
+ */
+ function getTimeFromTimezone(zone, offset) {
+    let date = new Date(new Date().toLocaleString("en-US", {timeZone: zone}))
+    var utc = date.getTime();
+    var nd = new Date(utc + (3600000*offset));
+    return nd
+}
+
+/**
+ * Increment Date by n hours
+ */
+function incrementDateHour(date, nHour) {
+    return new Date(moment(date).add(nHour, 'hours').format()).toLocaleString()
+}
+
+module.exports = {
+    getLongLatFromZipcode, getTimeFromTimezone, incrementDateHour
+}
