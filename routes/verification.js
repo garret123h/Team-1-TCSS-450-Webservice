5da046558e67c048721a53f8485f5d2a3ba99898
gt <gt@pop-os.localdomain>
2021-05-31 12:12:43 -1000

    Check verification code based off of email provided.
    This change is for when the client needs to check if the user is verified by hitting an endpoint with the email to see if they are verified

--- routes/verification.js
+++ routes/verification.js
@@ -90,4 +90,62 @@ router.put('/change-password', (request, response) => {
     }
 })
 
+/**
+ * @api {get} /check-verify/:email Check if a user has been verified
+ * @apiName VerifyUser
+ * @apiGroup Auth
+ * 
+ * @apiHeader {String} email "email" 
+ * 
+ * @apiSuccess {json} 
+ * 
+ *  * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 201 OK
+ *     {
+ *       "verify": true
+ *     }
+ * 
+ * @apiError (400: Missing Authorization Header) {String} message "Missing user"
+ * 
+ * @apiError (400: Malformed Authorization Header) {String} message "Malformed Authorization Header"
+ * 
+ * @apiError (404: User Not Found) {String} message "User not found"
+ * 
+ */
+router.get('/check-verify/:email', (request, response, next) => {
+    const email = request.params.email
+
+    if (isValidEmail(email)) {
+        let query = 'SELECT verification FROM MEMBERS WHERE email=$1'
+        let values = [email]
+        pool.query(query, values)
+            .then(result => {
+                if (result.rowCount == 0) {
+                    response.status(404).send({
+                        message: 'User not found'
+                    })
+                } else {
+                    let verifyResult = result.rows[0]
+                    if (verifyResult.verification == 0) {
+                        response.status(201).send({
+                            verify: "false"
+                        })
+                    } else {
+                        response.status(201).send({
+                            verify: "true"
+                        })
+                    }
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+                response.status(400).send()
+            })
+    } else {
+        response.status(400).send({
+            message: "Invalid Email!"
+        })
+    }
+})
+
 module.exports = router;
